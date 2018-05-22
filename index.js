7f81187bfa147d1173021b018874fd4108fcd7e9
Richard Melchior <raustin.melchior@gmail.com>
2018-05-22 00:09:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,3 +13,13 @@ function logShout(string){
 function logWhisper(string){
     console.log(whisper(string))
 }
+
+function sayHiToGrandma(string){
+  if(string.lowercase){
+    return "I can't hear you!"
+  } else if(string.uppercase) {
+    return "YES INDEED!"
+  } if(string === "I love you, Grandma."){
+    return "I love you, too."
+  }
+}
